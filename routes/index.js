34a93b614be9145820619fa22a36fb5e8676436c
Shinya Yamada <Shinya.Yamada@sony.com>
2018-03-09 19:35:18 +0900

    Devide template page to base page and header page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ItemSync' });
 });
+
+router.get('/create', function(req, res, next) {
+  res.render('create_channel', { title: 'ItemSync' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ItemSync' });
+});
+
+
 
 module.exports = router;
