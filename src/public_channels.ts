2d9c4ba9d24f83293e3d98270a8a5d3eaf117620
subaru44k <odsubaru@gmail.com>
2018-03-18 09:58:03 +0900

    Refactor public channels

--- src/public_channels.ts
+++ src/public_channels.ts
@@ -2,7 +2,9 @@ declare const firebase:any;
 
 import Vue from 'vue';
 import { FirebaseControl } from './model/firebasecontrol';
+import { FirebaseAuthControl } from './model/firebaseauthcontrol';
 import { Channel } from './item/channel';
+import NavbarComponent from './components/navbar';
 import ChannelItem from './components/channelitem';
 
 const channelList: Channel[] = [];
@@ -10,12 +12,40 @@ const channelList: Channel[] = [];
 const channelItem = new Vue({
     el: '#publicChannels',
     data: {
+        activeIndex: 0,
+        user: null,
         channels: channelList
     },
+    methods: {
+        handleLogout: function() {
+            firebaseAuthControl.signOut();
+        },
+        setUserData(user: any) {
+            this.user = user;
+        },
+        unsetUserData() {
+            this.user = null;
+        }
+    },
     components: {
+        'navigation-menu': NavbarComponent,
         'channel-item': ChannelItem
     }
 });
+
+function onSignin(user: any) {
+    console.log(user);
+    console.log(user.displayName);
+    console.log(user.email);
+    console.log(user.photoURL);
+    console.log(user.uid);
+    channelItem.setUserData(user);
+}
+
+function onSignout() {
+    console.log('signed out');
+    channelItem.unsetUserData();
+}
 
 const firebaseControl = new FirebaseControl(firebase);
 firebaseControl.getPublicChannels(10).then((channels) => {
@@ -23,3 +53,5 @@ firebaseControl.getPublicChannels(10).then((channels) => {
         channelList.push(channel);
     });
 });
+const firebaseAuthControl = new FirebaseAuthControl(firebase);
+firebaseAuthControl.startMonitoringSigninState(onSignin, onSignout);
