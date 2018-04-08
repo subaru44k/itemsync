4505b3d6225512711ffe3eebb3674244bbcfabe3
subaru44k <odsubaru@gmail.com>
2018-04-08 13:15:24 +0900

    Stop resetting input text when input box is clicked

--- src/components/todoitem.vue
+++ src/components/todoitem.vue
@@ -5,7 +5,7 @@
         img(src="/images/checked.svg", v-on:click.stop="onDeleteClicked(item)")
       .card-body
         h5.card-text.m-0.inline-block(v-if="!isInputVisible") {{ item.getContent() }}
-        input.inline-block(type="text", v-model="inputText", v-if="isInputVisible")
+        input.inline-block(type="text", v-model="inputText", v-if="isInputVisible", v-on:click.stop="onInputClicked")
         button.btn.btn-success(type='button', v-if="isInputVisible", v-on:click.stop="onUpdateClicked(item)") Update
         .right-align
           p.font-weght-light.font-italic.m-0
@@ -35,6 +35,10 @@
       private deleteItem(item: Item) {
           console.log('delete ' + item.getContent());
           this.$emit('delete-item-event', item);
+      }
+
+      onInputClicked() {
+        // nothing to do
       }
   
       onItemClicked(item: Item) {
